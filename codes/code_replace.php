30096fe3f2216064fd1e807181a7a06091700a88
y <yne870717@gmail.com>
2015-06-11 15:02:10 +0900

    change

--- codes/code_replace.php
+++ codes/code_replace.php
@@ -8,26 +8,26 @@ if (empty($dest_file)) {
   exit();
 }
 
-$file = file_get_contents($src_file, true);
+$data = file_get_contents($src_file, true);
 
 $pattern = '/^\s(.*?)\n(.*)/';
 $replacement = '$2';
-$file = preg_replace($pattern, $replacement, $file);
+$data = preg_replace($pattern, $replacement, $data);
 
 $pattern = '/0.*?:\s(.*)/';
 $replacement = '$1';
-$file = preg_replace($pattern, $replacement, $file);
+$data = preg_replace($pattern, $replacement, $data);
 
 $pattern = '/\s/';
 $replacement = '';
-$file = preg_replace($pattern, $replacement, $file);
+$data = preg_replace($pattern, $replacement, $data);
 
 $pattern = '/\n/';
 $replacement = '';
-$file = preg_replace($pattern, $replacement, $file);
+$data = preg_replace($pattern, $replacement, $data);
 
 $pattern = '/([0-9A-f]{2})/';
 $replacement = '0x$1, ';
-$file = preg_replace($pattern, $replacement, $file);
+$data = preg_replace($pattern, $replacement, $data);
 
-file_put_contents($dest_file, $file);
+file_put_contents($dest_file, $data);
